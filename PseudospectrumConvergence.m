d2208f6f20cef8a122ecff49841c3fef729dbb10
Yannick De Bruijn <yannick.debruijn23@gmail.com>
2025-11-11 21:21:51 +0100

    Update parameters and improve comments in code

--- PseudospectrumConvergence.m
+++ PseudospectrumConvergence.m
@@ -1,7 +1,7 @@
 %{
     --------------------------------------------------------------
     Author(s):    [Erik Orvehed HILTUNEN , Yannick DE BRUIJN]
-    Date:         [July 2025]
+    Date:         [November 2025]
     Description:  [Pseudospectrum convergence]
     --------------------------------------------------------------
 %}
@@ -13,13 +13,13 @@ clc;
 fs = 18;
 
 % --- Parameters ---
-    DimT = 50;     % Dimension Toeplitz "Operator" 
-    N = 20;        
+    DimT = 80;      % Dimension Toeplitz "Operator" 
+    N = 25;         % Truncation sizes range limit
     p = 1.4;        % Decay rate upwards
-    q = 3.6;        % Decay rate downwards
-    algebraic = 0;  
+    q = 1.4;        % Decay rate downwards (assume p<q)
+    algebraic = 1;  % 1 for alg. decay and 0 for exp. decay
 
-% --- Initialize vector to store residuals ---
+% --- Initialise vector to store residuals ---
     steps = 2: 1: N-1;
     residuals = zeros(length(steps), 1);
 
@@ -30,14 +30,14 @@ fs = 18;
         % --- Generate sequence a_k for n-banded ---
         col = zeros(n,1);
         row = zeros(1,n+1);
-        
         col(1) = 1; 
+        row(1) = 2;
     
-        % Inverval for coefficients [ai, bi]
+        % --- Add noise to the coefficients ---
         ai = 1;
         bi = 1;
         
-        % Downward decay (sub-diagonals)
+        % --- Downward decay (sub-diagonals) ---
         for k = 1:n
             r = ai + (bi-ai)*rand;
             if algebraic == 1
@@ -47,7 +47,7 @@ fs = 18;
             end
         end
         
-        % Upward decay (super-diagonals)
+        % --- Upward decay (super-diagonals) ---
         for k = 1:n
             r = ai + (bi-ai)*rand;
             if algebraic == 1
@@ -56,7 +56,6 @@ fs = 18;
                 row(k+1) = r * exp(-p*k);
             end
         end
-        row(1) = 1;
     
         colT = zeros(DimT, 1);
         colT(1) = 1;
@@ -71,18 +70,12 @@ fs = 18;
         end
         
         T = toeplitz(colT, rowT);
-
-        % Compute all eigenvalues and eigenvectors
         [V, D] = eig(T); 
     
-        selectEigval = 1;
-    
-        % Extract and sort eigenvalues
+        % --- Select middle eigenvector to mitigate edge effects ---
+        selectEigval = 20; 
         [lambda_sorted, sort_idx] = sort(diag(D), 'ascend');
-    
-        % Sort the eigenvectors accordingly
         V = V(:, sort_idx);
-    
         lambda = lambda_sorted(selectEigval);
         v = V(:, selectEigval);
         
@@ -113,8 +106,7 @@ fs = 18;
         % --- trendline for algebraic off-diagonal decay ---
         p = min(p,q);
         trend = zeta(p)  .* ( ( n_vals.^(-2*p) ) .* (n_vals.^p - 1) ) ./ (n_vals.^(p ./ n_vals) - 1);
-
-        trend = 0.5 * trend;
+        trend = 0.8 * trend;
     else 
         % --- trendline for exponential off-diagonal decay ---
         nonreciprocit = 0.5 * log ( row(2) / col(1) );
@@ -124,19 +116,21 @@ fs = 18;
         trend = constant * trend;
     end
     
-% --- Plot residuals ---
+% --- Plot residuals and the predicted trandline ---
     figure;
     if algebraic == 1
-        loglog(steps, residuals, '-o', 'LineWidth', 2);
+        loglog(steps, residuals, 'kx', 'LineWidth', 2);
     else
-        semilogy(steps, residuals, '-o', 'LineWidth', 2);
+        semilogy(steps, residuals, 'kx', 'LineWidth', 2);
     end
     hold on;
     plot(n_vals, trend, '--r', 'LineWidth', 2);
     
-    xlabel('Matrix size $N$', 'Interpreter', 'latex', 'FontSize', fs);
+    xlabel('Matrix size $N$', 'Interpreter', 'latex', 'FontSize', fs-1);
     ylabel('$\varepsilon_N$', 'Interpreter', 'latex', 'FontSize', fs);
-    set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', fs+2);
-    set(gcf, 'Position', [100, 100, 500, 300]);
+    legend('Numerical residual', 'Predicted trendline', 'Interpreter', 'latex', 'FontSize', fs, 'Location','southwest');
+    set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', fs);
+    set(gcf, 'Position', [100, 100, 500, 250]);
     grid on;
+
 
